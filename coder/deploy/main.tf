7b61e5bdc798ddb8f6bf363469c69280b35218d6
Aishwarya Bannimatti <aishbannimatti@gmail.com>
2025-08-21 19:19:19 -0400

    Added vpc, load balancer and ssl certificates creation to coder deployment terraform script

--- coder/deploy/main.tf
+++ coder/deploy/main.tf
@@ -23,6 +23,86 @@ resource "google_project_service" "compute_api" {
     disable_on_destroy = false
 }
 
+# Static IP address for the deployment. We'll assign this to our HTTPS load balancer later.
+resource "google_compute_address" "static_ip" {
+    name = "${var.project}-static-ip"
+    region = var.region
+    depends_on = [ google_project_service.compute_api ]
+
+    lifecycle {
+        prevent_destroy = true
+    }
+}
+
+# Next step is to set up our networking and an HTTPS load balancer.
+
+# Start with a new VPC (VPN?) network
+resource "google_compute_network" "vpn_network" {
+    name = "${var.project}-vpn"
+    auto_create_subnetworks = false
+}
+
+# Create a proxy-only subnet. This will be used by the HTTPS load balancer.
+resource "google_compute_subnetwork" "proxy_only" {
+    name = "${var.project}-proxy-subnet"
+    ip_cidr_range = "10.0.0.0/24"
+    region = var.region
+    network = google_compute_network.vpn_network.id
+    purpose = "REGIONAL_MANAGED_PROXY"
+    role = "ACTIVE"
+}
+
+# We need to add a router to allow outbound access to the public internet
+resource "google_compute_router" "nat_router" {
+    name    = "${var.project}-nat-router"
+    region  = var.region
+    network = google_compute_network.vpn_network.id
+}
+
+resource "google_compute_router_nat" "nat_config" {
+    name                               = "${var.project}-cloud-nat"
+    router                             = google_compute_router.nat_router.name
+    region                             = google_compute_router.nat_router.region
+    nat_ip_allocate_option             = "AUTO_ONLY"
+    source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
+
+# Firewall settings.
+# The IP ranges are for Google's internal network, these shouldn't change
+# TODO: Some of these IP ranges are not needed, test and trim them
+resource "google_compute_firewall" "firewall" {
+    name    = "${var.project}-firewall"
+    network = google_compute_network.vpn_network.name
+    allow {
+        protocol = "tcp"
+        ports    = ["22", "443", "3000"]
+    }
+    source_ranges = ["0.0.0.0/0", "35.235.240.0/20", "130.211.0.0/22", "35.191.0.0/16"]
+    depends_on = [ google_project_service.compute_api ]
+}
+
+# Create a separate private subnet. This will be used by the compute instance later.
+resource "google_compute_subnetwork" "private_subnet" {
+    name          = "${var.project}-subnet"
+    ip_cidr_range = "10.10.0.0/24"
+    region        = var.region
+    network       = google_compute_network.vpn_network.id
+    purpose       = "PRIVATE"  # default; can omit
+}
+
+# Create a health check, we'll add this on later
+resource "google_compute_health_check" "tcp_check" {
+    name = "tcp-health-check"
+    check_interval_sec = 5
+    timeout_sec = 5
+    healthy_threshold  = 2
+    unhealthy_threshold = 10
+
+    tcp_health_check {
+        port = 3000
+    }
+}
+
 # Create the compute instance
 resource "google_compute_instance" "server" {
     name         = var.vm_name
@@ -53,4 +133,66 @@ resource "google_compute_instance" "server" {
     depends_on = [ google_project_service.cloud_resource_manager_api, google_project_service.compute_api ]
 }
 
+# Next we need to set up the HTTPS load balancer. There are several steps to this.
 
+# Create an unmanaged instance group
+resource "google_compute_instance_group" "instance_group" {
+    name = "${var.project}-instance-group"
+    zone = var.zone
+
+    instances = [
+        google_compute_instance.server.self_link
+    ]
+
+    named_port {
+        name = "port-redirection"
+        port = 3000
+    }
+}
+
+# Create a backend service
+resource "google_compute_backend_service" "https_backend" {
+    name                  = "${var.project}-backend"
+    protocol              = "HTTP"
+    port_name             = "port-redirection"
+    timeout_sec           = 10
+    health_checks         = [google_compute_health_check.tcp_check.self_link]
+    load_balancing_scheme = "EXTERNAL"
+
+    backend {
+        group = google_compute_instance_group.instance_group.self_link
+    }
+}
+
+# At this point, we're ready to set up a HTTPS load balancer.
+resource "google_compute_url_map" "default" {
+    name            = "https-url-map"
+    default_service = google_compute_backend_service.https_backend.self_link
+}
+
+resource "google_compute_managed_ssl_certificate" "lb_default" {
+  provider = google-beta
+  name     = "myservice-ssl-cert"
+
+  managed {
+    domains = ["platform.vectorinstitute.ai"]
+  }
+}
+
+resource "google_compute_target_https_proxy" "default" {
+    name               = "${var.project}-https-proxy"
+    url_map            = google_compute_url_map.default.id
+    ssl_certificates   = [google_compute_managed_ssl_certificate.lb_default.id]
+}
+
+resource "google_compute_global_address" "default" {
+    name = "${var.project}-https-lb-ip"
+}
+
+resource "google_compute_global_forwarding_rule" "default" {
+    name                  = "${var.project}-https-forwarding-rule"
+    ip_address            = google_compute_global_address.default.address
+    port_range            = "443"
+    target                = google_compute_target_https_proxy.default.self_link
+    load_balancing_scheme = "EXTERNAL"
+}
