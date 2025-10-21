24962f2f4ac26bc3fef93fdf394decf45cb20a72
Amrit Krishnan <amrit110@gmail.com>
2025-10-21 13:40:30 -0400

    Set ssl_policy=null

--- coder/deploy/main.tf
+++ coder/deploy/main.tf
@@ -186,6 +186,7 @@ resource "google_compute_target_https_proxy" "default" {
   name             = "${var.project}-https-proxy"
   url_map          = google_compute_url_map.default.id
   ssl_certificates = [google_compute_managed_ssl_certificate.lb_default.id]
+  ssl_policy       = null
 }
 
 resource "google_compute_global_address" "default" {
